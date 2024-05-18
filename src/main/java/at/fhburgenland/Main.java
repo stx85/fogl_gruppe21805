c122ce4a4efbf90f20cdc1bfca257efde88729a4
Stefan Hahnl <2310859015@fh-burgenland.at>
2024-05-18 08:03:50 +0000

    initial commit

---
+++ src/main/java/at/fhburgenland/Main.java
@@ -0,0 +1,21 @@
+package at.fhburgenland;
+
+import jakarta.persistence.*;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+        /* TO DO
+            -) Connect Database
+            -) Klasse zur Tabelle erstellen!
+            -) Create Methods for
+                -) addPerson
+                -) readPerson
+                -) readAllPersons --> Ausgabe ganze Tabelle
+                -) update Person
+                -) delete Person
+         */
+    }
+}
